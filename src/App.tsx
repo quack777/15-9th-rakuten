0034f5e64007c034df4832d59953b4f5f8e45745
choi2601 <byeonghyeonchoe55@gmail.com>
2022-02-24 17:47:36 +0900

    Add: API 통신 기능 및 router 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Container from "components/Container";
 import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
+import { getItemData } from './api/getItemData';
 
 function App() {
+
+  useEffect(() => {
+    async function getData() {
+      const result = await getItemData();
+
+      console.log(result);
+    }
+
+    getData();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
       <Container>
-        {/* <LinkPage /> */}
-        <DetailPage />
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<LinkPage />}/>
+        <Route path="/detail/:id" element={<DetailPage />}/>
+        </Routes>
+        </BrowserRouter>
       </Container>
     </>
   );
---
+++ src/api/getItemData.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export interface ItemType {
+    created_at:     number;
+    key:            string;
+    expires_at:     number;
+    download_count: number;
+    count:          number;
+    size:           number;
+    summary:        string;
+    thumbnailUrl:   string;
+    files:          File[];
+    sent?:          Sent;
+}
+
+export interface File {
+    key:          string;
+    thumbnailUrl: string;
+    name:         string;
+    size:         number;
+}
+
+export interface Sent {
+    subject: string;
+    content: string;
+    emails:  string[];
+}
+
+export const getItemData = async (): Promise<ItemType[]> => {
+    const { data } = await axios.get('http://localhost:3000/data/itemInfoList.json');
+    
+    return data;
+}
